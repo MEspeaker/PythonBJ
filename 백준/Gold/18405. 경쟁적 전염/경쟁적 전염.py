11f87e4e070f39a709f47f4e593922b1caba11a9
MEspeaker <92416864+MEspeaker@users.noreply.github.com>
2025-05-14 17:41:10 +0900

    [Gold V] Title: 경쟁적 전염, Time: 140 ms, Memory: 35572 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,32 +1,31 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-dr = [1, -1, 0, 0]
-dc = [0, 0, 1, -1]
+dr=[1,-1,0,0] #아래,위
+dc=[0,0,1,-1] #오른쪽,왼쪽
 
-n, k = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(n)]
-s, x, y = map(int, input().split())
+n,k=map(int,input().rstrip().split())
+matrix=[list(map(int, input().rstrip().split())) for _ in range(n)]
+s,x,y=map(int,input().rstrip().split())
 
-q = []
+q=[]
 for i in range(n):
-    for j in range(n):
-        if matrix[i][j] != 0:
-            q.append((matrix[i][j], i, j, 0))  # (virus_type, row, col, time)
+  for j in range(n):
+    if matrix[i][j]!=0:
+      q.append((matrix[i][j],i,j,0)) #바이러스값,행,열,시간
 
-q.sort()  
-q = deque(q)
+q.sort()
+q=deque(q)
 
 while q:
-    virus, r, c, time = q.popleft()
-    if time == s:
-        continue
-    for d in range(4):
-        nr = r + dr[d]
-        nc = c + dc[d]
-        if 0 <= nr < n and 0 <= nc < n and matrix[nr][nc] == 0:
-            matrix[nr][nc] = virus
-            q.append((virus, nr, nc, time + 1))
-
-print(matrix[x - 1][y - 1])
+  v,row,col,t=q.popleft()
+  if t==s:
+    continue
+  for l in range(4):
+    r= row+dr[l]
+    c= col+dc[l]
+    if 0<= r < n and 0<= c < n and matrix[r][c]==0:
+      matrix[r][c]=v
+      q.append((v,r,c,t+1))
+print(matrix[x-1][y-1])
